78a6398e28749c931d83e4807eb3138a48dbd7b3
Serafin Lüthi <serafin.luethi@outlook.com>
2022-09-19 14:23:47 +0200

    create NetworkHandler as singleton

---
+++ app/src/main/java/ch/teko/weather_app/NetworkHandler.java
@@ -0,0 +1,37 @@
+package ch.teko.weather_app;
+
+import android.net.ConnectivityManager;
+import android.net.Network;
+
+import androidx.annotation.NonNull;
+
+public class NetworkHandler extends ConnectivityManager.NetworkCallback {
+
+    private static NetworkHandler networkHandler;
+
+    public boolean isAvaliable = false;
+
+    private NetworkHandler() {
+        // keep it private
+    }
+
+    public static NetworkHandler getInstance() {
+        if (networkHandler == null) {
+            networkHandler = new NetworkHandler();
+        }
+        return networkHandler;
+    }
+
+
+    @Override
+    public void onAvailable(@NonNull Network network) {
+        super.onAvailable(network);
+        isAvaliable = true;
+    }
+
+    @Override
+    public void onLost(@NonNull Network network) {
+        super.onLost(network);
+        isAvaliable = false;
+    }
+}
